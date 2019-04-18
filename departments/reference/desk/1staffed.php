f8ef1ca035b5b8791e8931fc71c115734b17c318
Josh <jddocea@gmail.com>
2019-04-18 11:37:58 -0500

    bug squashing

--- departments/reference/desk/1staffed.php
+++ departments/reference/desk/1staffed.php
@@ -25,10 +25,10 @@
     <h5>Currently staffed by</h5>
     <h2 class="no-margin-top center"><?php echo $Staff_Name?></h2>
     <p>
-      <?php echo "<a href=/forms/email/index.php?id=$Staff_Email>" ?>
+      <?php echo "<a style=text-decoration:none href=/forms/email/index.php?id=$Staff_Email>" ?>
       <img align="absmiddle" src="/img/email.png" alt="email" title="contact via email"/>
       </a>
-      <i> | <?php echo $Staff_Phone_Number ?></i>
+      <i> | 815-939-5355 </i>
     </p>
     <p>
       Our reference staff sit at the reference desk in order to assist with research and
