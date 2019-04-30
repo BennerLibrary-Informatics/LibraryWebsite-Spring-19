eeb4c52daa25796cf9cdc4d45722c630ae53ae6f
jmlundahl <jmlundahl@olivet.edu>
2019-04-30 11:16:08 -0500

    Moved to status folder

---
+++ departments/reference/desk/status/info_student.php
@@ -0,0 +1,15 @@
+  <div class="split l30-r70 cf">
+    <div class="left center">
+    <img src="/departments/reference/desk/img/student-staff.png" alt="graphic" />
+    </div>
+
+    <div class="right center">
+      <h5 class="no-margin-top center">Currently staffed by</h5>
+      <h2>Informatics Student</h2>
+      <p><em>815-939-5355</em></p>
+      <p>
+        Informatic students can assist you with printing needs and minor technology needs.
+        They can take a message.
+      </p>
+    </div>
+  </div>
---
+++ departments/reference/desk/status/no_staff.php
@@ -0,0 +1,24 @@
+  <div class="split l30-r70 cf padding30">
+    <div class="left center">
+      <a href="/forms/email/index.php?id=009">
+        <img src="/departments/reference/desk/img/email-librarian.png" alt="graphic" />
+      </a>
+    </div>
+
+    <div class="right center">
+      <h5>Reference Desk is not currently staffed</h5>
+      <h2 class="no-margin-top center">Email a Library Staff member for assistance</h2>
+      <p>
+        <a style="text-decoration:none" href="/forms/email/index.php?id=009">
+          <img align="absmiddle" src="/img/email.png" alt="email" title="contact via email"/>
+        </a>
+        <i> | 815-939-5355</i>
+      </p>
+      <p>
+        Even though there is no one currently staffing the reference desk,
+        our website is open 24/7.
+        Go to the Help tab on the homepage for self-help links or use the email
+        link above to contact a staff member who will return your email within 48 hours.
+      </p>
+    </div>
+  </div>
