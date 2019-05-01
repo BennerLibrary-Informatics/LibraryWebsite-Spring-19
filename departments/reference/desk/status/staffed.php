2fe2b831cd75d3d7ca630581edc64adc7d78fb72
Josh <jddocea@gmail.com>
2019-05-01 14:03:20 -0500

    updated path to pictures

--- departments/reference/desk/status/staffed.php
+++ departments/reference/desk/status/staffed.php
@@ -2,8 +2,8 @@
 <div class="split l30-r70 cf">
   <div class="left center">
       <?php
-        if (file_exists('./img/staff/'.$username.'.jpg')){
-          echo "<img style=position:absolute src=/departments/reference/desk/img/staff/$username.jpg alt=graphic />";
+        if (file_exists('../../../img/staff/'.$username.'.jpg')){
+          echo "<img style=position:absolute src=/img/staff/$username.jpg alt=graphic />";
           echo "<img style=position:relative src=/departments/reference/desk/img/at-desk-frame.png />";
 				} else {
             echo "<img src=/img/staff/photo_filler.jpg />";
@@ -15,7 +15,7 @@
     <h5>Currently staffed by</h5>
     <h2 class="no-margin-top center"><?php echo $Staff_Name?></h2>
     <p>
-      <a style=text-decoration:none href=/forms/email/index.php?id=009>
+      <a style="text-decoration:none" href="/forms/email/index.php?id=009">
       <img align="absmiddle" src="/img/email.png" alt="email" title="contact via email"/>
       </a>
       <i> | 815-939-5355 </i>
